3658223844718f6d59d52cec18ccd702779c0edd
Manea Luca-Mihail <71531456+kennyass1@users.noreply.github.com>
2024-11-06 00:46:42 +0200

    Add files via upload

---
+++ Lab1.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+int main()
+{
+    /*
+    int x;
+    scanf("%d",&x);
+    printf("%d %o %x",2*x);
+    int lungime,Latime;
+    scanf("%d %d",&lungime,&Latime);
+    printf("perimetru %d",2*lungime+2*Latime);
+    */
+    char caracter;
+    scanf("%c",&caracter);
+    printf("%c \n",caracter);
+    printf("%c %c \n",caracter,caracter);
+    printf("%c %c %c",caracter,caracter,caracter);
+    return 0;
+}
